f635a14c440dd3d0ed5fe58b9b5805ebbb0f6373
kt6delta <lcpreciadob@udistrital.edu.co>
2024-06-29 19:37:40 -0500

    title y alerts

---
+++ src/components/reusable/table.tsx
@@ -0,0 +1,7 @@
+export const Table = () => {
+    return (
+        <>
+            <h1 className="text-secondary  justify-center w-full text-xl sm:justify-end"></h1>
+        </>
+    )
+}
--- src/components/reusable/user/FooterPage.tsx
+++ src/components/reusable/user/FooterPage.tsx
@@ -1,7 +1,7 @@
 export const FooterPage = () => {
     return (
         <>
-            <div className='bg-default text-background flex justify-center w-full min-h-[7vh] rounded-2xl
+            <div className='bg-primary text-background flex justify-center w-full min-h-[7vh] rounded-2xl
                 sm:justify-end'>
             </div>
         </>
