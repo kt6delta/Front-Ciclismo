78e4128c53545a4179b4489a0b519d66560ca834
kt6delta <lcpreciadob@udistrital.edu.co>
2024-07-15 12:20:11 -0500

    se agrego ciclista a la animacion

--- src/components/simulation/Progreso.jsx
+++ src/components/simulation/Progreso.jsx
@@ -1,17 +1,28 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Imagen from "@/components/simulation/Imagen";
 import { IoIosBicycle } from "react-icons/io";
+import { Title } from "@/components/reusable/title";
 export default function Progreso() {
+  const [progress, setProgress] = useState(0);
   useEffect(() => {
     import('progressbar.js').then(ProgressBar => {
       var bar = new ProgressBar.default.Path('#heart-path', {
         easing: 'easeInOut',
         duration: 10000,
-        step: function(state, path, attachment) {
-          console.log('Carrera en progreso:'+state);
-          <IoIosBicycle className="w-12 h-12" />
-      }
+        step: function (state, path, attachment) {
+          if(path.value()<0.25){
+            setProgress((path.value()*-1)*1.75);
+          } else if(path.value()>=0.25 && path.value()<0.37){
+            setProgress((path.value()-0.5)*1.75);
+          }else if(path.value()>=0.37 && path.value()<0.5 ){
+            setProgress((path.value()-0.5)*1.75);
+          }else if(path.value()>=0.5 && path.value()<0.74){
+            setProgress((path.value()-0.5)*1.75);
+          }else if(path.value()>=0.74 && path.value()<=1){
+            setProgress(((path.value()*-1)+1)*1.75);
+          } 
+        }
       });
       bar.set(0);
       bar.animate(1, {
@@ -20,8 +31,18 @@ export default function Progreso() {
       });
     });
   }, []);
+
+  let iconStyle = {
+    transform: `translateX(${progress * 100}%)`
+  };
   return (
     <>
+      <div className="flex justify-center items-center mb-20">
+        <Title mesage="CARRERA" />
+      </div>
+      <div className="flex justify-center items-center" style={iconStyle}>
+        <IoIosBicycle className="w-12 h-12" />
+      </div>
       <div className="flex justify-center items-center">
         <Imagen />
       </div>
